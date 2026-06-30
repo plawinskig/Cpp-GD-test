5b2c334bce87f09aae1d869a803890912cc47744
Grzegorz Pławiński <90200788+Greggy55@users.noreply.github.com>
2026-06-30 22:22:38 +0200

    Refactor variable names for consistency and readability in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,16 @@
 
 int main() 
 {
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-    
-    InitWindow(screenWidth, screenHeight, "Game of snake");
+    const int screen_width = 800;
+    const int screen_height = 600;
 
-    Texture2D playerTex = LoadTexture("assets/player.png");
+    const int cell_size = 100;
+    const int cell_count_x = 10;
+    const int cell_count_y = 10;
+    
+    InitWindow(screen_width, screen_height, "Game of snake");
+
+    Texture2D player_tex = LoadTexture("assets/player.png");
 
     SetTargetFPS(60);
 
@@ -17,16 +21,16 @@ int main()
         
         ClearBackground(RAYWHITE);
 
-        DrawTexture(playerTex, 100, 100, WHITE);
+        DrawTexture(player_tex, 100, 100, WHITE);
         
-        DrawTextureEx(playerTex, {400.0f, 100.0f}, 0.0f, 3.0f, WHITE);
+        DrawTextureEx(player_tex, {400.0f, 100.0f}, 0.0f, 3.0f, WHITE);
 
         DrawText("Player one", 100, 500, 20, DARKGRAY);
 
         EndDrawing();
     }
 
-    UnloadTexture(playerTex);
+    UnloadTexture(player_tex);
     
     CloseWindow();
 
