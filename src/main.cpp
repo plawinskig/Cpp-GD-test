c60c52264abdac699e54969c1494d7569e7ea4f9
Grzegorz Pławiński <90200788+Greggy55@users.noreply.github.com>
2026-06-30 22:39:38 +0200

    Update background colors and improve drawing logic in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@ const int CELL_SIZE = 100;
 const int CELL_COUNT_X = 10;
 const int CELL_COUNT_Y = 9;
 
+const Color DARK_GRAY = {50, 50, 50, 255};
+const Color LIGHT_GRAY = {70, 70, 70, 255};
+
 void drawCell(int pos_x, int pos_y, Color col)
 {
     DrawRectangle(pos_x * CELL_SIZE, pos_y * CELL_SIZE, CELL_SIZE, CELL_SIZE, col);
@@ -18,11 +21,19 @@ int main()
     while (!WindowShouldClose()) 
     {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(DARK_GRAY);
 
-        Color col = {200, 100, 100, 255};
-        drawCell(0, 0, col);
-        drawCell(2, 1, col);
+        // background
+        for (int i = 0; i < CELL_COUNT_X; ++i)
+        {
+            for (int j = 0; j < CELL_COUNT_Y; ++j)
+            {
+                if ((i + j) % 2 == 1)
+                {
+                    drawCell(i, j, LIGHT_GRAY);
+                }
+            }
+        }
         
         EndDrawing();
     }
