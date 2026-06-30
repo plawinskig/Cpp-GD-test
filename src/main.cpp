925f61dfdfd8e9a9000b70416deb6db44173b876
Grzegorz Pławiński <90200788+Greggy55@users.noreply.github.com>
2026-06-30 22:33:31 +0200

    Remove player texture loading and drawing logic for simplification

--- src/main.cpp
+++ src/main.cpp
@@ -15,27 +15,20 @@ int main()
     const int screen_height = 600;
 
     InitWindow(screen_width, screen_height, "Game of snake");
-
-    Texture2D player_tex = LoadTexture("assets/player.png");
-
+    
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) 
     {
         BeginDrawing();
-        
         ClearBackground(RAYWHITE);
 
-        DrawTexture(player_tex, 100, 100, WHITE);
+        Color col = {200, 100, 100, 255};
+        drawCell(0, 0, col);
+        drawCell(2, 1, col);
         
-        DrawTextureEx(player_tex, {400.0f, 100.0f}, 0.0f, 3.0f, WHITE);
-
-        DrawText("Player one", 100, 500, 20, DARKGRAY);
-
         EndDrawing();
     }
-
-    UnloadTexture(player_tex);
     
     CloseWindow();
 
