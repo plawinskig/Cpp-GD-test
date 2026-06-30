55178286b032c14595abae22049fc9f148157534
Grzegorz Pławiński <90200788+Greggy55@users.noreply.github.com>
2026-06-30 22:26:43 +0200

    Move cell size and count constants to the top and add drawCell function for better structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,19 @@
 #include "raylib.h"
 
+const int cell_size = 100;
+const int cell_count_x = 10;
+const int cell_count_y = 10;
+
+void drawCell(int pos_x, int pos_y, Color col)
+{
+    DrawRectangle(pos_x * cell_size, pos_y * cell_size, cell_size, cell_size, col);
+}
+
 int main() 
 {
     const int screen_width = 800;
     const int screen_height = 600;
 
-    const int cell_size = 100;
-    const int cell_count_x = 10;
-    const int cell_count_y = 10;
-    
     InitWindow(screen_width, screen_height, "Game of snake");
 
     Texture2D player_tex = LoadTexture("assets/player.png");
