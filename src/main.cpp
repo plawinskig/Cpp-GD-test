803b380f1a1b2da353faf278d279930438a57434
Grzegorz Pławiński <90200788+Greggy55@users.noreply.github.com>
2026-06-30 20:28:23 +0200

    Replace console output with raylib window initialization and rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
+#include "raylib.h"
 
-int main() {
-    std::cout << "Reddit" << std::endl;
+int main() 
+{
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+    InitWindow(screenWidth, screenHeight, "GD test window");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) 
+    { 
+        BeginDrawing();
+        {
+            ClearBackground(RAYWHITE);
+
+            DrawText("The Raylib programmer", 150, 280, 20, DARKGRAY);
+
+            DrawCircle(400, 350, 30, RED);
+        }
+        EndDrawing();
+    }
+
+    CloseWindow(); 
+
     return 0;
 }
