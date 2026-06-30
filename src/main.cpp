fd9342ad96809cd1621b70139e192b386dfb71c9
Grzegorz Pławiński <90200788+Greggy55@users.noreply.github.com>
2026-06-30 20:33:40 +0200

    Refactor main loop to load and draw player texture, improving graphics rendering

--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,30 @@ int main()
 {
     const int screenWidth = 800;
     const int screenHeight = 600;
+    
     InitWindow(screenWidth, screenHeight, "GD test window");
+
+    Texture2D playerTex = LoadTexture("assets/player.png");
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) 
-    { 
+    {
         BeginDrawing();
-
         ClearBackground(RAYWHITE);
 
-        DrawText("The Raylib programmer", 150, 280, 20, DARKGRAY);
-
-        DrawCircle(400, 350, 30, RED);
+        DrawTexture(playerTex, 100, 100, WHITE);
         
+        DrawTextureEx(playerTex, {400.0f, 100.0f}, 0.0f, 3.0f, WHITE);
+
+        DrawText("Grafika zaladowana pomyslnie!", 100, 500, 20, DARKGRAY);
+
         EndDrawing();
     }
 
-    CloseWindow(); 
+    UnloadTexture(playerTex);
+    
+    CloseWindow();
 
     return 0;
 }
